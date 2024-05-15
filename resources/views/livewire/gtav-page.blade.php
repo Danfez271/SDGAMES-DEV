9440a93480c139e61df053abc61950e2f4ad2809
Daniel Yepez <danfez27@gmail.com>
2024-05-15 15:45:25 -0400

    PLANTILLA GAMES & RESPONSIVE

--- resources/views/livewire/gtav-page.blade.php
+++ resources/views/livewire/gtav-page.blade.php
@@ -3,10 +3,10 @@
         <div>
             <div class="relative">
 
-                <header class="bg-[url(../../public/images/homepage/pagpopulares/bannergtapagjuego.png)]  bg-cover min-h-[800px] h-[20vw] bg-center md:min-h-[40vw]" >
+                <header class="bg-[url(../../public/images/homepage/pagpopulares/bannergtapagjuego.png)] lg:bg-cover  min-h-[800px] h-[20vw] bg-center md:min-h-[40vw] md:bg-cover xs:bg-cover xs bg-no-repeat xs:min-h-[600px] " >
                     <div class="bg-black opacity-50 h-full ">
 
-                        <nav class="nav mx-20 justify-self-center w-11/12 max-w-[1200px] truncate font-bold " id="nav">
+                        <nav class="nav sm:mx-20 justify-self-center xs:mx-[20%]  max-w-[1200px] truncate font-bold  " id="nav">
 
 
                             <h2 class="nav__logo ">
@@ -20,16 +20,16 @@
                             <ul class="nav__links  ">
 
                                 <li class="nav__item">
-                                    <a href="#" class="nav__link hover:text-[#02F2EB] ">Inicio</a>
+                                    <a href="#" class="nav__link ">Inicio</a>
                                 </li>
                                 <li class="nav__item">
-                                    <a href="#" class="nav__link hover:text-[#02F2EB]">Catalogo</a>
+                                    <a href="#" class="nav__link ">Catalogo</a>
                                 </li>
                                 <li class="nav__item">
-                                    <a href="#" class="nav__link hover:text-[#02F2EB]">Promociones</a>
+                                    <a href="#" class="nav__link ">Promociones</a>
                                 </li>
                                 <li class="nav__item">
-                                    <a href="#" class="nav__link hover:text-[#02F2EB]">Contacto</a>
+                                    <a href="#" class="nav__link ">Contacto</a>
                                 </li>
 
 
@@ -57,20 +57,67 @@
         </div>
     </div>
 {{--    CONTENIDO --}}
-    <div class="bg-black min-h-[1600px]">
-        <div class="flex-col">
-            <h1 class="text-8xl text-white font-semibold ml-20 mt-20 mb-5 ">GTA V</h1>
+    <div class="bg-black min-h-[1300px]   ">
+        <div class="lg:grid lg:grid-cols-2 ">
 
-            <p class="text-white text-2xl font-extralight ml-20 ">Te damos la bienvenida a Los Santos <br><br>
+            <div>
+                <h1 class="text-8xl text-white font-semibold mt-20 mb-5 lg:ml-20 lg:text-start xs:mx-5 sm:text-center xs:text-center ">GTA V</h1>
 
-                Un joven estafador callejero, un ladrón de bancos retirado y
-                un psicópata aterrador se ven involucrados con lo peor y más
-                desquiciado del mundo criminal, del gobierno de los EE. UU.
-                y de la industria del espectáculo, y tendrán que llevar a cabo
-                una serie de peligrosos golpes para sobrevivir en una ciudad
-                implacable en la que no pueden confiar en nadie. Y mucho
-                menos los unos en los otros.</p>
+                <p class="text-white text-2xl font-extralight sm:ml-20 mr-24 lg:text-xl sm:text-xl xs:text-lg xs:mx-10 ">Te damos la bienvenida a Los Santos <br><br>
+
+                    Un joven estafador callejero, un ladrón de bancos retirado y
+                    un psicópata aterrador se ven involucrados con lo peor y más
+                    desquiciado del mundo criminal, del gobierno de los EE. UU.
+                    y de la industria del espectáculo, y tendrán que llevar a cabo
+                    una serie de peligrosos golpes para sobrevivir en una ciudad
+                    implacable en la que no pueden confiar en nadie. Y mucho
+                    menos los unos en los otros.</p>
+
+                <h2 class="text-8xl text-white font-light lg:ml-28 mt-10 mb-5 md:text-center lg:text-start sm:text-center xs:text-center xs:text-7xl  ">
+                    0.00$
+                </h2>
+
+                <div class=" flex flex-wrap 2xl:gap-4 lg:gap-4 xl:gap-4 md:gap-4 sm:gap-0 xs:gap-0 my-auto lg:ml-24 mt-8 md:text-center lg:text-start xs:text-center ">
+                    <div class="flex-1 2xl:basis-0 lg:basis-0 md:basis-0 xs:basis-full sm:basis-full ">
+                        <a href="#" id="" class="2xl:text-3xl xl:text-3xl lg:text-4xl md:text-4xl sm:text-4xl xs:text-2xl  text-white font-bold  p-3.5 sm:mt-5   text-center rounded-full bg-gradient-to-r from-[#00FFF6] to-[#09B2B2] px-14 hover:text-white  hover:bg-gradient-to-r hover:from-[#A200FF] hover:to-[#A200FF]  ">
+                            Añadir al carrito
+                        </a>
+                    </div>
+                </div>
+
+            </div>
+            <div>
+                <iframe class="lg:w-[90%] lg:h-[460px] sm:mt-44 ml-4 mr-2 sm:w-[90%] sm:h-[400px] sm:ml-12 sm:mr-4 lg:ml-4 xs:mx-[10vw] xs:mt-32 xs:w-[80%] xs:h-[200px]     " src="https://www.youtube.com/embed/hvoD7ehZPcM?si=rezyiKSlN0kAITy7&amp;start=1" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
+            </div>
         </div>
+
+        <div class="sm:grid sm:grid-cols-3 ml-14 mr-14 mt-20 mb-5 sm:scale-100">
+
+            <div>
+                <img src="{{asset('images/homepage/pagpopulares/1.png')}}" alt="">
+            </div>
+            <div>
+                <img src="{{asset('images/homepage/pagpopulares/2.png')}}" alt="">
+            </div>
+            <div>
+                <img src="{{asset('images/homepage/pagpopulares/3.png')}}" alt="">
+            </div>
+
+        </div>
+
+        <div class="grid grid-cols-1 text-center">
+
+            <div class=" flex flex-wrap 2xl:gap-4 lg:gap-4 xl:gap-4 md:gap-4 sm:gap-0 xs:gap-0 my-auto  mt-16 mb-20 ">
+                <div class="flex-1 2xl:basis-0 lg:basis-0 md:basis-0 xs:basis-full sm:basis-full ">
+                    <a href="#" id="" class="2xl:text-3xl xl:text-3xl lg:text-4xl md:text-4xl sm:text-4xl xs:text-sm  text-white font-bold  p-3.5 mt-5  text-center rounded-full bg-gradient-to-r from-[#00FFF6] to-[#09B2B2] px-14 hover:text-white  hover:bg-gradient-to-r hover:from-[#A200FF] hover:to-[#A200FF]  ">
+                        Ver todos los juegos
+                    </a>
+                </div>
+            </div>
+
+        </div>
+
+
 
 
 
